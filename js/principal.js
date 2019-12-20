b6bc62e8aafb0e39261196f43a01befb935f09d3
Laudelino Martins Cardoso Neto <laudelino@farolshopping.com.br>
2019-12-20 12:51:30 -0300

    Início da Programação
    Início da programação de rotinas de cálculo de IMC.

---
+++ js/principal.js
@@ -0,0 +1,27 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "**Aparecida Nutricionista**";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
+console.log(altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+
+if (peso <= 0 || peso >= 1000){
+	console.log("Peso inválido");
+	tdImc.textContent = "Peso inválido";
+} else if (altura <= 0 || altura >= 3){
+	console.log("Altura inválida");
+	tdImc.textContent = "Altura inválida";
+} else {
+	var imc = peso / (altura * altura);
+	console.log(imc);
+
+	tdImc.textContent = imc;
+}	
