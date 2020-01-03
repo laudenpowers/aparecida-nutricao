b7ffd28e24899fb27001ef91813bbaf0aa2c4ed7
Laudelino Martins Cardoso Neto <laudelino@farolshopping.com.br>
2020-01-03 08:15:55 -0300

    Módulo 3
    Continuação dos estudos no módulo 3

--- js/principal.js
+++ js/principal.js
@@ -22,6 +22,33 @@ if (peso <= 0 || peso >= 1000){
 } else {
 	var imc = peso / (altura * altura);
 	console.log(imc);
-
 	tdImc.textContent = imc;
+}
+
+
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
+
+for (var i = 0; i < pacientes.length; i++){
+	
+	var paciente = pacientes[i];
+	var tdPeso = paciente.querySelector(".info-peso");
+	var tdAltura = paciente.querySelector(".info-altura");
+	var peso = tdPeso.textContent;
+	var altura = tdAltura.textContent;	
+
+	var tdImc = paciente.querySelector(".info-imc");
+
+	if (peso <= 0 || peso >= 1000){
+		console.log("Peso inválido");
+		tdImc.textContent = "Peso inválido";
+	} else if (altura <= 0 || altura >= 3){
+		console.log("Altura inválida");
+		tdImc.textContent = "Altura inválida";
+	} else {
+		var imc = peso / (altura * altura);
+		console.log(imc);
+		tdImc.textContent = imc.toFixed(2);
+	}
+	
 }	
