e336544915ea5cf746723609ddf989bc9b56a8b5
Laudelino Martins Cardoso Neto <laudelino@farolshopping.com.br>
2020-01-11 16:18:08 -0300

    Continuação
    Continuação do curso, onde foi apresentado como manipular CSS via javascript.

--- js/principal.js
+++ js/principal.js
@@ -42,9 +42,15 @@ for (var i = 0; i < pacientes.length; i++){
 	if (peso <= 0 || peso >= 1000){
 		console.log("Peso inválido");
 		tdImc.textContent = "Peso inválido";
+		paciente.classList.add("paciente-invalido");
+		//paciente.style.color = "white";
+		//paciente.style.backgroundColor = "lightcoral";
 	} else if (altura <= 0 || altura >= 3){
 		console.log("Altura inválida");
 		tdImc.textContent = "Altura inválida";
+		paciente.classList.add("paciente-invalido");
+		//paciente.style.color = "white";
+		//paciente.style.backgroundColor = "lightcoral";
 	} else {
 		var imc = peso / (altura * altura);
 		console.log(imc);
@@ -52,3 +58,20 @@ for (var i = 0; i < pacientes.length; i++){
 	}
 	
 }	
+
+var btnAdicionar = document.querySelector("#adicionar-paciente");
+btnAdicionar.addEventListener("click", function (event){
+	event.preventDefault();
+	console.log("Botão clicado");
+});
+
+titulo.addEventListener("click", showMsg);
+
+function showMsg(){
+	console.log("Fui clicado")
+}
+
+titulo.addEventListener("click", function (){
+	console.log("Fui clicado (função anomima)");
+});
+
